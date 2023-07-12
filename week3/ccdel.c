a5438d21d8af0f90230e337f822c25378733eaf8
ColeBardin <cab572@drexel.edu>
2023-07-12 16:45:11 -0400

    wrote ccdel.c

---
+++ week3/ccdel.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/file.h>
+#include "cc.h"
+
+int main(int argc, char *argv[]){
+	CComp oldcomp;
+	FILE *fp;
+	int id;
+	
+	if(argc != 2){
+		fprintf(stderr, "Usage: ccdel id\n");
+		exit(1);
+	}
+	id = atoi(argv[1]);
+	
+	fp = fopen("ccdb", "r+");
+	if(fp == NULL){
+		if(errno == ENOENT){
+			fp = fopen("ccdb", "w+");
+			if(fp == NULL){
+				perror("fopen");
+				exit(2);
+			}
+		}
+	}
+	flock(fileno(fp), LOCK_EX);
+	fseek(fp, id * sizeof(CComp), SEEK_SET);
+	fread(&oldcomp, sizeof(CComp), 1, fp);
+
+	memset(oldcomp.name, 0, Nname);
+	memset(oldcomp.maker, 0, Nmaker);
+	memset(oldcomp.cpu, 0, Ncpu);
+	oldcomp.year = 0;
+	oldcomp.id = 0;
+	memset(oldcomp.desc, 0, Ndesc);
+
+	fseek(fp, id * sizeof(CComp), SEEK_SET);
+	fwrite(&oldcomp, sizeof(CComp), 1, fp);
+
+	flock(fileno(fp), LOCK_UN);	
+	fclose(fp);
+}
+
