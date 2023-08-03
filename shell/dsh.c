35a5dea5af9b4a1f29bf3e075cdc50aae906c0a7
ColeBardin <cab572@drexel.edu>
2023-08-03 10:38:41 -0400

    commented dsh source code

--- shell/dsh.c
+++ shell/dsh.c
@@ -8,9 +8,34 @@
 #define MAXTOKS 128
 #define BUFSIZE 256
 
+/**
+ Tokenizes a string by whitespace.
+ @param s String to tokenize
+ @param toks buffer where tokens will be stored
+ @param maxtoks size of toks array
+ @return number of tokens found
+ */
 int tokenize(char *s, char *toks[], int maxtoks);
+
+/**
+ Function body for exit builtin command.
+ @param argv Argument vector
+ @retun 0
+ */
 int exitCmd(char *argv[]);
+
+/**
+ Funciton body for cd builtin command.
+ @param argv Argument vector
+ @return 0
+ */
 int cdCmd(char *argv[]);
+
+/**
+ Function body for pwd builtin command.
+ @param argv Argument vector
+ @return 0
+ */
 int pwdCmd(char *argv[]);
 
 Command cmds[] = {
@@ -22,28 +47,31 @@ const int Ncmds = sizeof(cmds) / sizeof(Command);
 char line[BUFSIZE];
 
 int main(void){
-	char *curDir;
 	int cmd;
 	int n;
-	char *toks[MAXTOKS];
-	pid_t CHPID;
 	int state;
+	char *toks[MAXTOKS];
+	char *curDir;
+	pid_t CHPID;
 
 	while(1){
+		// Build basic prompt
 		getcwd(line, BUFSIZE);
 		curDir = strrchr(line, '/');
 		if(strlen(curDir) != 1){
 			curDir++;
 		}
-		
 		printf("%s D$ ", curDir);
+
+		// Read and tokenie user input
 		if(fgets(line, BUFSIZE, stdin) == NULL){
 			exit(0);
 		}
-		
+		// Skip empty lines
+		if(line[0] == '\n') continue;
 		n = tokenize(line, toks, MAXTOKS);	
-		if(n == 0) return -1;
 
+		// Search through builtins and execute if found
 		for(cmd = 0; cmd < Ncmds && strncmp(toks[0], cmds[cmd].name, strlen(toks[0])); cmd++);
 		if(cmd < Ncmds){
 			cmds[cmd].f(toks);
@@ -53,7 +81,7 @@ int main(void){
 			continue;
 		}
 
-		// Create child process and run argument vector
+		// If not a builtin, fork and exec arbitrary command
 		CHPID = fork();
 		if(CHPID == 0){
 			if(execvp(toks[0], toks) == -1){
@@ -73,7 +101,8 @@ int tokenize(char *s, char *toks[], int maxtoks){
 	char *p;
 
 	i = 0;
-	toks[i] = strtok(s, " ");
+	// Tokenize by whitespace
+	toks[i] = strtok(s, " \t");
 	while(toks[i] != NULL){
 		p = strchr(toks[i], '\n');
 		if(p != NULL){
@@ -83,7 +112,7 @@ int tokenize(char *s, char *toks[], int maxtoks){
 		if(i >= maxtoks - 1){
 			toks[i] = NULL;
 		}else{
-			toks[i] = strtok(NULL, " ");
+			toks[i] = strtok(NULL, " \t");
 		}
 	}
 	return i;
@@ -96,15 +125,18 @@ int exitCmd(char *argv[]){
 
 int cdCmd(char *argv[]){
 	char *p;
-	if(argv[1] == NULL || strlen(argv[1]) == 0){
+	if(argv[1] == NULL || argv[1][0] == '\0'){
+		// Empty argument: go to home directory
 		p = getenv("HOME");
 	}else{
+		// Remove newline from arg
 		p = strchr(argv[1], '\n');
 		if(p != NULL){
 			*p = '\0';
 		}
 		p = argv[1];
 	}
+	// Change dir
 	if(chdir(p) < 0){
 		perror(line);
 	}
--- shell/dsh.h
+++ shell/dsh.h
@@ -1,8 +1,7 @@
-#ifndef _CSH_H_
-#define _CSH_H_
+#ifndef _DSH_H_
+#define _DSH_H_
 
 typedef struct Command Command;
-
 struct Command {
 	char *name;
 	int (*f)(char *argv[]);
