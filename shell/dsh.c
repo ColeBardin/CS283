322dfe0aa7181c9720e9366a419c5692dc4e18a0
ColeBardin <cab572@drexel.edu>
2023-08-10 20:57:13 -0400

    added file redirection. need to refactor >

--- shell/dsh.c
+++ shell/dsh.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "dsh.h"
 
 #define MAXTOKS 128
@@ -51,9 +53,12 @@ Command cmds[] = {
 };
 const int Ncmds = sizeof(cmds) / sizeof(Command);
 char line[BUFSIZE];
+int fdi, fdo;
 
 int main(void){
 	char *curDir;
+	char *p;
+	int i, len;
 
 	while(1){
 		// Build basic prompt
@@ -68,18 +73,112 @@ int main(void){
 		if(fgets(line, BUFSIZE, stdin) == NULL){
 			exit(0);
 		}
-		// Skip empty lines
-		if(line[0] == '\n') continue;
+		// Remove newline from line
+		p = strchr(line, '\n');
+		if(p != NULL) *p = '\0';
 	
-		if(handleRedir(line) == 0) break;
+		// Ingore leading whitespace from string
+		len = strlen(line);
+		for(i = 0; i < len; i++){
+			if(line[i] != ' ' && line[i] != '\t'){
+				p = &line[i];
+				break;	
+			}
+		}	
+		// Skip empty lines
+		if(i == len) continue;
+
+		if(handleRedir(p) == 0) break;
 	}
 	
 	exit(0);	
 }
 
 int handleRedir(char *line){
-		
-	return handleCmd(line);	
+	int ret;
+	char *in, *out;
+	char *p;
+	char buf[BUFSIZE];
+	int i, j, len;
+	int truncate;
+	int flag;
+
+	fdi = -1;
+	fdo = -1;	
+
+	// Search for input redir
+	in = strchr(line, '<');
+	if(in != NULL){
+		p = in++;
+		// Set in ptr to first non whitespace character after <
+		while(*in == ' ' || *in == '\t') in++;
+		len = strlen(in);
+		// Find end of argument for redir
+		for(i = 0; i < len; i++){
+			if(in[i] == ' ' || in[i] == '\t' || in[i] == '>') break;
+			if(in[i] <= ' ' || in[i] > '~') break;
+		}
+		// Copy filename into buffer
+		memcpy(buf, in, i);
+		buf[i] = '\0';
+		// Remove redir from line
+		for(j = 0; j < in - p + i; j++){
+			p[j] = ' ';
+		}
+		fdi = open(buf, O_RDONLY);
+		if(fdi == -1){
+			fprintf(stderr, "Error opening '%s': ", buf);
+			perror("");
+			return -1;
+		}
+	}
+
+	// Search for output redir
+	out = strchr(line, '>');
+	if(out != NULL){
+		truncate = 1; 
+		p = out;
+		out = strchr(line, '>');
+		if(out != NULL){
+			// Second > char, appending type
+			truncate = 0;
+		}else{
+			out = p;
+		}		
+		out++;
+
+		// Set in ptr to first non whitespace character after <
+		while(*out == ' ' || *out == '\t') out++;
+		len = strlen(out);
+		// Find end of argument for redir
+		for(i = 0; i < len; i++){
+			if(out[i] == ' ' || out[i] == '\t' || out[i] == '<') break;
+			if(out[i] <= ' ' || out[i] > '~') break;
+		}
+		// Copy filename into buffer
+		memcpy(buf, out, i);
+		buf[i] = '\0';
+		// Remove redir from line
+		for(j = 0; j < out - p + i; j++){
+			p[j] = ' ';
+		}
+	
+		if(truncate == 0){
+			flag = O_WRONLY | O_CREAT | O_APPEND;
+		}else{
+			flag = O_WRONLY | O_CREAT | O_TRUNC;
+		}
+		fdo = open(buf, flag, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		if(fdo == -1){
+			fprintf(stderr, "Error opening '%s': ", buf);
+			perror("");
+			return -1;
+		}
+	}
+	// cmd<f1>f2
+	ret = handleCmd(line);	
+	
+	return ret;
 }
 
 int handleCmd(char *line){
@@ -89,6 +188,7 @@ int handleCmd(char *line){
 	
 	// Tokenize command
 	n = tokenize(line, toks, MAXTOKS);	
+	if(n == 0) return -1;
 
 	// Search through builtins or execute arbitrary command
 	for(cmd = 0; cmd < Ncmds - 1 && strncmp(toks[0], cmds[cmd].name, strlen(toks[0])); cmd++);
@@ -152,6 +252,12 @@ int runCmd(int argc, char *argv[]){
 
 	CHPID = fork();
 	if(CHPID == 0){
+		if(fdi != -1) dup2(fdi, 0);
+		if(fdo != -1) dup2(fdo, 1);
+		if(fdo != -1) dup2(fdo, 2);
+
+		close(fdi);
+		close(fdo);
 		execvp(argv[0], argv);
 		printf("Command not found: %s\n", argv[0]);
 		exit(1);
